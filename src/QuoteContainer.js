1013a3a8b01bcc323b9e8324c671954d661654f7
jamesncox <jamesncox@yahoo.com>
2020-04-27 16:07:41 -0500

    Update QuoteContainer component

---
+++ src/QuoteContainer.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getQuote } from './actions/quotes'
+import Quote from './Quote'
+
+class QuoteContainer extends Component {
+
+    handleClick = () => {
+        this.props.getQuote()
+    }
+
+    render() {
+        return (
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <img
+                            src="https://bigmemes.funnyjunk.com/pictures/Albert_e4f2e8_6546489.jpg"
+                            alt="Avatar"
+                            style={{ borderRadius: ".1in" }}
+                        />
+                    </div>
+                    <div className="flip-card-back">
+                        <button onClick={this.handleClick}>Get Quote</button>
+                        <Quote />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = (state) => ({
+    quote: state.quote
+})
+
+export default connect(mapStateToProps, { getQuote })(QuoteContainer)
