a6491c4c2f9a6c9962890c9440f59dd7bc70d6e5
jamesncox <jamesncox@yahoo.com>
2020-04-29 17:45:19 -0500

    Update index.html and app.js new title

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="wrapper">
-          <h1>QUOTIVATIONAL</h1>
+          <h1>"quotivational"</h1>
           <h2>Need motivation? Click below to see a famous quote</h2>
           {/* <Signup /> */}
           <QuoteContainer />
